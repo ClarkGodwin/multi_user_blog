b2e1d187f02f06e7ba4c0399f433be4156e85460
ClarkGodwin <clarkniyonzima@gmail.com>
2026-08-14 08:19:49 +0200

    last before pause

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static \Database\Factories\ArticleFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Article newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Article newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Article query()
+ * @mixin \Eloquent
+ */
 class Article extends Model
 {
     /** @use HasFactory<\Database\Factories\ArticlesFactory> */
