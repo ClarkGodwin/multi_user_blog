d9bf30130e4acb36ba5ba4e0a576b889e6ec39a2
ClarkGodwin <clarkniyonzima@gmail.com>
2026-08-14 07:31:42 +0200

    types updated in dbmode

--- database/migrations/2026_08_14_052712_create_articles_table.php
+++ database/migrations/2026_08_14_052712_create_articles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
             $table->timestamps();
         });
     }
