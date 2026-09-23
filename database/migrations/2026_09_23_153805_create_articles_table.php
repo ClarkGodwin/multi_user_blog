e51dc5841c8452055245b773c78a01582e5aeacc
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-23 17:43:16 +0200

    article's migration file done'

--- database/migrations/2026_09_23_153805_create_articles_table.php
+++ database/migrations/2026_09_23_153805_create_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\ArticleStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->nullable(false);
+            $table->string('title')->nullable(false);
+            $table->text('content')->nullable(false);
+            $table->string('status')->default(ArticleStatusEnum::Craft->value);
             $table->timestamps();
         });
     }
