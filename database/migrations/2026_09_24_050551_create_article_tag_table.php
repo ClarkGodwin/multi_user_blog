d36999e675e648a917495d4236e612d52cf091f5
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-24 07:11:43 +0200

    articletag's migration  file done

--- database/migrations/2026_09_24_050551_create_article_tag_table.php
+++ database/migrations/2026_09_24_050551_create_article_tag_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('article_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('article_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->nullable(false);
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate()->nullable(false);
             $table->timestamps();
         });
     }
