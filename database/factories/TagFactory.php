1d16e183d79167465450127f4274739e227edd76
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-24 07:13:01 +0200

    tag's factory file done

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,8 @@ class TagFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "user_id" => User::inRandomOrder('id')->first()->id,
+            "name" => $this->faker->word,
         ];
     }
 }
