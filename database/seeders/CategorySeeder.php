50febfcf0265e9abc7975d8540b86ea1bd22ec7d
ClarkGodwin <clarkniyonzima@gmail.com>
2026-09-24 06:48:16 +0200

    category's seeder file done

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Database\Factories\ArticleCategoryFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,7 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::factory()->count(10)->create();
+        ArticleCategoryFactory::new()->count(30)->create();
     }
 }
